b5717bba9abb4f89adc9c08d9c108c2f922ddf5b
Juan Ignacio Lopez Rosa <lopezjuanignacion@gmail.com>
2022-05-26 20:01:01 -0300

    ll

--- CLASE 6/ejemplo.js
+++
@@ -1,1 +0,0 @@
-let etiqueta = prompt('etiqueta a cambiar')
---
+++ CLASE 7/ejemplo.js
@@ -0,0 +1,4 @@
+let etiqueta = prompt('etiqueta a cambiar')
+let etiqueta2 = prompt('nuevo valor')
+var damevalor = document.getElementById(etiqueta)
+damevalor.innerText=(etiqueta2)
