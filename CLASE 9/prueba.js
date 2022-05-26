e4623d95975e16f038ac1fa9b6bc8be7b0bd641f
Juan Ignacio Lopez Rosa <lopezjuanignacion@gmail.com>
2022-05-26 20:00:55 -0300

    .

---
+++ CLASE 9/prueba.js
@@ -0,0 +1,7 @@
+function saldoTotal(){
+   let n1 = document.getElementById('1');
+   let n2 = document.getElementById('2');
+   let n3 = document.getElementById('3');
+   var sum = n1 + n2 + n3;
+   console.log("$" , sum);
+}
