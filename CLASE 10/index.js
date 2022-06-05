bdaf877acdc740d5bac6eadbc18f08117431e5af
Juan Ignacio Lopez Rosa <lopezjuanignacion@gmail.com>
2022-06-04 22:11:16 -0300

    .

--- CLASE 10/index.js
+++ CLASE 10/index.js
@@ -11,8 +11,11 @@ const dibujar = document.getElementById('div1');
 
 btnDibujar.onclick= function(){
     let textoarreglado = textarea.value.replaceAll('\n','<br>');
-    console.log(textarea.value);
+    // console.log(textarea.value);
     let nueva = document.createElement('p')
-    nueva.innerHTML = textoarreglado;
+    // nueva.innerHTML = textoarreglado
+    let pesos = textarea.value.indexOf('$');
+    let resultado = textarea.value.slice(pesos+1, pesos+3);
+    nueva.innerHTML = resultado;
     dibujar.appendChild(nueva)
 }
