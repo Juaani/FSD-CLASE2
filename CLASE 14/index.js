2ea57b569138997f5c36f3a59c7aea1707f727f3
Juan Ignacio Lopez Rosa <lopezjuanignacion@gmail.com>
2022-06-11 21:14:40 -0300

    l

---
+++ CLASE 14/index.js
@@ -0,0 +1,14 @@
+let transacciones = [];
+let nombres = [];
+let gastos = [];
+let gastosTotales = [];
+
+let transaccion = {
+    nombre,
+    gasto,
+}
+transacciones.push(transaccion)
+
+function agregarTransacciones(){
+    for()
+}
