bbad23da17568a4ad42504ef9daf19c327f0d590
Juan Ignacio Lopez Rosa <lopezjuanignacion@gmail.com>
2022-07-13 00:23:48 -0300

    l

--- CLASE 21/index.py
+++ CLASE 21/index.py
@@ -2,4 +2,25 @@ def saludar():
     """Devuelve siempre 1"""
     x=1
 
+entrada = int(input("Mensaje: ")) #entrada por teclado
+
+entrada = list(input("Mensaje: ")) #entrada de lista
+
+entrada = tuple(input("Mensaje: ")) #lista no modificable
+
+x, y, z = [1,2,3] #asignar datos
+data = []
+while True:
+    line = int(input("mensaje: "))
+    if line:
+        data.append(int(line))
+    else:
+        break
+
+#if else determinado por la identacion no por el ;
+
+def saludo(nombre):
+    print(f"Hola {nombre}")
+
+#funciones
 
